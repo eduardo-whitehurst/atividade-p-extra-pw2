07b51a1b7b69f24423607e47a68865119925d9d5
João Darwin <joaodarwin.ip22@gmail.com>
2025-02-20 13:39:36 -0300

    feat: Implementar rotas da aplicaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Cart from "./pages/Cart"
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/login" Component={Login} />
+        <Route path="/cart" Component={Cart} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
