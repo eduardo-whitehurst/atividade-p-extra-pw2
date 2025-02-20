c67801baa73b4fe0610b709f8ca05b2dc7e909db
Eduardo Dênis Paiva Whitehurst <202012010035@ad.ifpb.edu.br>
2025-02-20 15:39:30 -0300

    Feat: Colocando o controle de estado nos inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,11 +2,22 @@ import { MapPinIcon } from '@heroicons/react/24/solid';
 import logo from "../../public/images/logo/logo-ifpb-vertical.png";
 import burguer from "../../public/images/logo/logo.png";
 import "@fontsource/baloo-2/latin.css";
-
+import { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+    const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSenhaChange = (e) => {
+    setSenha(e.target.value);
+  };
+
   return (
-    <div className='bg-[#F1E9C9] h-screen w-screen '>
+    <div className='bg-[#F1E9C9] h-screen w-screen'>
       <div className="flex flex-row justify-around gap-96 p-4 items-center">
         <img src={logo} alt="ifpb logo" className='w-25 h-auto'/>
         <div className='flex flex-row gap-1'>
@@ -16,7 +27,7 @@ const Login = () => {
       </div>
 
       <div className='flex justify-center items-center'>
-        <div className='flex flex-col justify-center '>
+        <div className='flex flex-col justify-center'>
           <div className=' mt-10 w-2xl'>
             <h2 className='text-5xl font-baloo font-bold text-[#403937]'>Bem vindos ao IFPB Lanches</h2>
             <p className='text-xl text-[#8D8686]'>Seu lanche no IFPB de forma rápida e prática! Acesse sua conta ou cadastre-se e aproveite a melhor experiência.</p>
@@ -24,16 +35,28 @@ const Login = () => {
 
           <div className='flex items-center'>
             <div className='pt-3'>
-              <div className='flex flex-col gap-4 w-lg justify-center items-center '>
-              <div className='flex flex-col w-full'>
+              <div className='flex flex-col gap-4 w-lg justify-center items-center'>
+                <div className='flex flex-col w-full'>
                   <form className='flex flex-col gap-2.5 w-full'>
                     <div>
                       <label className='font-bold'>Email</label>
-                      <input type="email" className='w-full h-9 bg-white pl-4 rounded-md' placeholder='Digite seu email'/>
+                      <input
+                        type="email"
+                        className='w-full h-9 bg-white pl-4 rounded-md'
+                        placeholder='Digite seu email'
+                        value={email} 
+                        onChange={handleEmailChange} 
+                      />
                     </div>
                     <div>
                       <label className='font-bold'>Senha</label>
-                      <input type="password" className='w-full h-9 bg-white pl-4 rounded-md' placeholder='Digite seu email'/>
+                      <input
+                        type="password"
+                        className='w-full h-9 bg-white pl-4 rounded-md'
+                        placeholder='Digite sua senha'
+                        value={senha} 
+                        onChange={handleSenhaChange} 
+                      />
                     </div>
                     <button className='bg-[#34C759] w-full h-11 rounded-md text-white font-bold mt-8'>Login</button>
                     <button className='bg-[#FFCC00] w-full h-11 rounded-md text-[#8D8686] font-bold'>Login com Google</button>
@@ -46,15 +69,10 @@ const Login = () => {
               <img src={burguer} alt="hamburguer" />
             </div>
           </div>
-
         </div>
       </div>
-
-
-
-
-      </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
