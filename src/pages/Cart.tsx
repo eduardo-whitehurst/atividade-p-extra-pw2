4d4096a2c55c6a4bb28c070deeeb1a9cac832208
Matheus Gomes <79982182+Matheus9557@users.noreply.github.com>
2025-02-20 16:23:48 -0300

    Atualizando o Cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,37 +1,39 @@
-import FormEnderecoEntrega from '../components/formEnderecoEntrega'
-import CardPagamento from '../components/CardPagamento'
-import { HeaderPage } from '../components/Header'
-import OrderSummary from '../components/LanchesSelecionados'
+import { HeaderPage } from "../components/Header";
+import FormEnderecoEntrega from "../components/formEnderecoEntrega";
+import CardPagamento from "../components/CardPagamento";
+import LanchesSelecionados from "../components/LanchesSelecionados";
 
 const Cart = () => {
-    return (
-        <div>
-            <HeaderPage />
-            <div
-                className="mt-6 w-4/5 mx-auto"
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between"
-                }}
-            >
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: 50
-                    }}
-                >
-                    <div>
-                        <h2 className="text-lg font-bold mb-4">Complete seu pedido</h2>
-                        <FormEnderecoEntrega />
-                    </div>
-                    <CardPagamento />
-                </div>
-                <OrderSummary />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center w-full min-h-screen bg-gray-50">
+      {/* Header */}
+      <HeaderPage />
 
-export default Cart
+      {/* Conteúdo principal */}
+      <div className="mt-10 grid grid-cols-3 gap-8 w-4/5">
+        {/* Coluna esquerda - Endereço e Pagamento */}
+        <div className="col-span-2 flex flex-col gap-8">
+          {/* Título acima do formulário de entrega */}
+          <div className="text-[#3f3937] text-lg font-bold font-['Baloo 2'] leading-normal">
+            Complete seu pedido
+          </div>
+
+          <FormEnderecoEntrega />
+          <CardPagamento />
+        </div>
+
+        {/* Coluna direita - Resumo do Pedido */}
+        <div className="flex flex-col gap-6">
+          {/* Título acima do carrinho de compras */}
+          <div className="text-[#3f3937] text-lg font-bold font-['Baloo 2'] leading-normal">
+            Lanches selecionados
+          </div>
+
+          <LanchesSelecionados />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
